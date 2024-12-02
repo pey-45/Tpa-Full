d69e817223bb4a5b6d66e9c5d056783546b51e84
pey-45 <pablo.manzanares.lopez@udc.es>
2024-12-02 12:58:54 +0100

    Mod project template

---
+++ src/client/java/tpapey/TpapeyClient.java
@@ -0,0 +1,10 @@
+package tpapey;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TpapeyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
